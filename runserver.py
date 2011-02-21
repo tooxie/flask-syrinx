7c52c0424369452a1bc4bb1dd3aeda962c2bf38c
Alvaro Mouriño <alvaro@mourino.net>
2011-02-21 20:12:29 -0200

    Creates DB if it doesn't exist and checks for SECRET_KEY.

--- runserver.py
+++ runserver.py
@@ -5,6 +5,11 @@ from os import path
 
 db_path = path.join(path.dirname(path.abspath(__file__)), 'syrinx/syrinx.db')
 if not path.exists(db_path):
+    print(' * Database created: %s' % db_path)
     db.create_all()
+
+if not app.config['SECRET_KEY']:
+    raise ConfigError('Missing SECRET_KEY. Run utils.secret_key_gen() to ' + \
+                      'generate one.')
 
 app.run(debug=True)
