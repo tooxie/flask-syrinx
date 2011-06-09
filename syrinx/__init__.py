bf0f57cd27a15426cfb982b1ac801c2c9b3cbbc0
Alvaro Mouriño <alvaro@mourino.net>
2011-06-09 00:14:02 -0300

    Added author, changed version number and minor changes.

--- syrinx/__init__.py
+++ syrinx/__init__.py
@@ -5,27 +5,27 @@ Syrinx
 
 A microblogging application written with Flask.
 """
-from .exceptions import ConfigError
+from syrinx.exceptions import ConfigError
+
 from datetime import datetime
 from flask import Flask
 from flaskext.script import Manager, Server
-from hashlib import md5
+import hashlib
 import logging
 import sys
 
-__version__ = '0.1.1'
+__author__ = u'Alvaro Mouriño <alvaro@mourino.net>'
 __flask_version__ = '0.6.1'
+__version__ = '0.1.2'
 
 try:
-    from settings import *
+    from syrinx import settings
 except ImportError:
-    # cp settings.py-customize settings.py
-    logging.warning('Could not find local settings. Aborting...')
+    logging.error('ERROR: Could not find settings. Aborting...')
     sys.exit(1)
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('SYRINX_SETTINGS', silent=True)
+app.config.from_object(settings)
 
 manager = Manager(app)
 manager.add_command("runserver", Server())
@@ -39,15 +39,14 @@ def format_datetime(timestamp):
 def gravatar_url(email, size=80):
     """Return the gravatar image for the given email address."""
     return 'http://www.gravatar.com/avatar/%s?d=identicon&s=%d' % \
-        (md5(email.strip().lower().encode('utf-8')).hexdigest(), size)
+        (hashlib.md5(email.strip().lower().encode('utf-8')).hexdigest(), size)
 
 # add some filters to jinja
 app.jinja_env.filters['datetimeformat'] = format_datetime
 app.jinja_env.filters['gravatar'] = gravatar_url
 
-import views
-import models
+from syrinx import models
 
 if not app.config['SECRET_KEY']:
-    logging.warning('Missing SECRET_KEY. Run utils.secret_key_gen() to ' + \
-                    'generate one.')
+    logging.warning(' '.join('Missing settings.SECRET_KEY. Run'
+                             'utils.crypto.secret_key_gen() to generate one.'))
