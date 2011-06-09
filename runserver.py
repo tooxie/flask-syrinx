5b25d71fa1a2181e983b6d047647ad8d7cdcc18a
Alvaro Mouriño <alvaro@mourino.net>
2011-06-09 00:12:15 -0300

    Style changes.

--- runserver.py
+++ runserver.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from syrinx import app
 from syrinx.models import db
+
 from os import path
 
 db_path = path.join(path.dirname(path.abspath(__file__)), 'syrinx/syrinx.db')
@@ -9,7 +10,7 @@ if not path.exists(db_path):
     db.create_all()
 
 if not app.config['SECRET_KEY']:
-    raise ConfigError('Missing SECRET_KEY. Run utils.secret_key_gen() to ' + \
-                      'generate one.')
+    raise ConfigError(''.join('Missing SECRET_KEY. Run syrinx.utils.',
+                              'secret_key_gen() to generate one.'))
 
 app.run(debug=True)
